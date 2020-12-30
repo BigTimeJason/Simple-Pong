dde32778259fa6a051befa778e0213be3fbe18b7
BigTimeJason <49235257+BigTimeJason@users.noreply.github.com>
2020-12-30 17:53:19 +0000

    Init commit

---
+++ Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    private bool started = false;
+
+    public AudioSource hitSound, blipSound;
+
+    public TextMeshProUGUI textMeshPro;
+
+    public Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!started)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                Launch();
+                started = true;
+                textMeshPro.enabled = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Player":
+                hitSound.Play();
+                break;
+            case "Untagged":
+                blipSound.Play();
+                break;
+        }
+    }
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
+        textMeshPro.enabled = true;
+        started = false;
+    }
+
+    private void Launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.velocity = new Vector2(speed * x, speed * y);
+    }
+}
---
+++ Pong/Assets/Scripts/Goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public bool isPlayer1Goal;
+
+    public AudioSource scoreSound;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            scoreSound.Play();
+            if (isPlayer1Goal)
+            {
+                GameObject.Find("GameManager").GetComponent<GameManager>().Player2Scored();
+            } else
+            {
+                GameObject.Find("GameManager").GetComponent<GameManager>().Player1Scored();
+            }
+        }
+    }
+}
